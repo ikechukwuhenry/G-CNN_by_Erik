376618c950e47a45baa4e6b3c55e97eacc19c87c
Ikechukwu Michael <ikechukwuhenry@ymail.com>
2024-11-13 13:02:32 +0100

    add a new method for finding the group element inverse)

--- cyclic_group.py
+++ cyclic_group.py
@@ -13,9 +13,25 @@ class C4:
             return (valid_r + valid_s) % 4
         except ValueError:
             print("Invalid Input")
-            
+    
     @staticmethod
     def inverse(r: int) -> int:
+        # Implements the *inverse* operation of the group C_4.
+        # The input `r` must be an integer in {0, 1, 2, 3} and represents an element of the group.
+        # The method should return the integer representing the inverse of input element.
+        # You should also check that the input is valid.
+
+        ### BEGIN SOLUTION
+        try:
+            valid_r = int(r)
+            return (4 - valid_r) % 4
+        except ValueError:
+            print('Invalid input')
+
+        ### END SOLUTION
+    
+    @staticmethod
+    def inverse_method2(r: int) -> int:
         try:
             valid_r = int(r)
             for n in range(4):
@@ -23,7 +39,6 @@ class C4:
                     return n
         except ValueError:
             print("Invalid Input")
-
 
 # Testing with invalid input
 print(C4.product(1, 'a'))
@@ -35,4 +50,4 @@ print(C4.product(2, 3))
 # assert C4.product(0, 0) == 0
 # assert C4.product(2, 3) == 1
 print("group inverse element")
-print(C4.inverse(2))
+print(C4.inverse(3))
--- dihedral_group.py
+++ dihedral_group.py
@@ -9,10 +9,17 @@ class D4:
         # You should also check that the inputs are valid.
 
         ### BEGIN SOLUTION
-
+        f_a, r_a = a
+        f_b, r_b = b
+        try:
+            valid_f_a = int(f_a)
+            valid_r_a = int(r_a)
+            valid_f_b = int(f_b)
+            valid_r_b = int(r_b)
+        except ValueError:
+            print("Invalid Inputs") 
 
         ### END SOLUTION
-        pass
 
     @staticmethod
     def inverse(g: tuple) -> tuple:
